27704954d1dbd91b32e451a756b46cbfb397dab4
Sourav Saini <sainisourav279@gmail.com>
2026-04-17 00:33:09 +0530

    second_largest_element_in_array

--- array.cpp
+++ array.cpp
@@ -17,6 +17,51 @@ int largest_element_in_array(int arr[], int n)
     return largest;
 }
 
+int second_largest_element_in_array(int arr[], int n)
+{
+    // Brute force method (less optimal)
+    // sort(arr, arr + n);
+    // int i = n - 1;
+    // while (i--)
+    // {
+    //     if (arr[i] < arr[n - 1])
+    //         return arr[i];
+    // }
+    // return -1;
+
+    // Better solution
+    // int largest = arr[0];
+    // int second_largest = INT_MIN;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (arr[i] > largest)
+    //         largest = arr[i];
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (arr[i] > second_largest && arr[i] < largest)
+    //         second_largest = arr[i];
+    // }
+    // return second_largest;
+
+    // Most optimal solution best
+    int largest = arr[0];
+    int second_largest = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > second_largest && arr[i] < largest)
+        {
+            second_largest = arr[i];
+        }
+        if (arr[i] > largest)
+        {
+            second_largest = largest;
+            largest = arr[i];
+        }
+    }
+    return second_largest;
+}
+
 int main()
 {
     int n;
@@ -26,6 +71,8 @@ int main()
     {
         cin >> arr[i];
     }
-    int largest_element = largest_element_in_array(arr, n);
-    cout << "Largest Element: " << largest_element;
+    int second_largest_element = second_largest_element_in_array(arr, n);
+    cout << "Second Largest Element: " << second_largest_element;
+    // int largest_element = largest_element_in_array(arr, n);
+    // cout << "Largest Element: " << largest_element;
 }
